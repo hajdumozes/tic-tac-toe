1b21c50a305f1ba3f04e5b59b1b78e2743f56ed7
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-05-07 11:26:31 +0200

    parse to array index

--- src/main/java/component/UserInputParser.java
+++ src/main/java/component/UserInputParser.java
@@ -6,7 +6,9 @@ import java.util.regex.Pattern;
 import model.Cell;
 
 public class UserInputParser {
-    private static final Pattern ACCEPTABLE_PATTERN = Pattern.compile("[0-9]");
+    private static final char MIN_RANGE = '1';
+    private static final char MAX_RANGE = '9';
+    private static final Pattern ACCEPTABLE_PATTERN = Pattern.compile(String.format("[%s-%s]", MIN_RANGE, MAX_RANGE));
 
     public Cell askForInput() {
         System.out.println();
@@ -14,7 +16,7 @@ public class UserInputParser {
         int row = parseInput();
         printAskForColumn();
         int column = parseInput();
-        return new Cell(row, column);
+        return of(row, column);
     }
 
     private void printAskForRow() {
@@ -31,8 +33,14 @@ public class UserInputParser {
             String input = scanner.next(ACCEPTABLE_PATTERN);
             return Integer.parseInt(input);
         } else {
-            System.out.println("Wrong parameter, please use a single number.");
+            System.out.printf("Wrong parameter, please use a single number between %s and %s%n", MIN_RANGE, MAX_RANGE);
             return parseInput();
         }
+    }
+
+    private Cell of(int userInputRow, int userInputColumn) {
+        int rowInArray = userInputRow - 1;
+        int columnInArray = userInputColumn - 1;
+        return new Cell(rowInArray, columnInArray);
     }
 }
